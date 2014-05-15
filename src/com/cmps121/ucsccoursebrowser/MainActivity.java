53ef5eb2af92391213691bdca8fce7bf7c443ac9
Maximillian Laumeister <max.laumeister@gmail.com>
2014-05-14 19:13:06 -0700

    Main activity layout

--- src/com/cmps121/ucsccoursebrowser/MainActivity.java
+++ src/com/cmps121/ucsccoursebrowser/MainActivity.java
@@ -1,18 +1,31 @@
 package com.cmps121.ucsccoursebrowser;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
+	ListView listViewSearch;
+	List<Map<String, String>> listData = new ArrayList<Map<String, String>>();
+	SimpleAdapter listAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +35,24 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new SearchFragment()).commit();
 		}
+		
+		listViewSearch = (ListView) findViewById(R.id.listViewSearch);
+		Log.d("CourseBrowser", listViewSearch.toString());
+		initListViewSearch();
+	}
+	
+	private void initListViewSearch() {
+		for (int i = 0; i < 3; i++) {
+			Map<String, String> datum = new HashMap<String, String>(2);
+			datum.put("First Line", "First Line " + Integer.toString(i));
+			datum.put("Second Line","Second Line " + Integer.toString(i));
+			listData.add(datum);
+		}
+		listAdapter = new SimpleAdapter(this, listData,
+				android.R.layout.simple_list_item_2, 
+				new String[] {"First Line", "Second Line" }, 
+				new int[] {android.R.id.text1, android.R.id.text2 });
+		listViewSearch.setAdapter(listAdapter);
 	}
 
 	@Override
