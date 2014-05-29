641c501098d3a28201c4ffcd7d9344f5bfa815d7
Maximillian Laumeister <max.laumeister@gmail.com>
2014-05-29 00:02:01 -0700

    Reformat search parameters

--- src/com/cmps121/ucsccoursebrowser/PisaHTMLModel.java
+++ src/com/cmps121/ucsccoursebrowser/PisaHTMLModel.java
@@ -14,15 +14,15 @@ public class PisaHTMLModel {
 	public static final ArrayList<SearchParameter> SEARCH_PARAMETERS = new ArrayList<SearchParameter>();
 	static {
 		ArrayList<SearchParameter> s = SEARCH_PARAMETERS;
-		//							#label						#type					#html_id
-		s.add(new SearchParameter(	"Term", 					FieldType.MULT_CHOICE, 	"term_dropdown"		));
-		s.add(new SearchParameter(	"Status", 					FieldType.MULT_CHOICE, 	"reg_status"		));
-		s.add(new SearchParameter(	"Subject", 					FieldType.MULT_CHOICE, 	"subject"			));
-		s.add(new SearchParameter(	"Course Title Keyword", 	FieldType.TEXT_ENTRY, 	"title"				));
-		s.add(new SearchParameter(	"Instructor Last Name", 	FieldType.TEXT_ENTRY, 	"instructor"		));
-		s.add(new SearchParameter(	"General Education", 		FieldType.MULT_CHOICE, 	"ge"				));
-		s.add(new SearchParameter(	"Course Units", 			FieldType.TEXT_ENTRY, 	"crse_units_exact"	));
-		s.add(new SearchParameter(	"Meeting Days", 			FieldType.MULT_CHOICE, 	"Days"				));
-		s.add(new SearchParameter(	"Meeting Times", 			FieldType.MULT_CHOICE, 	"Times"				));
+		//                        #label                  #type                  #html_id
+		s.add(new SearchParameter("Term",                 FieldType.MULT_CHOICE, "term_dropdown"));
+		s.add(new SearchParameter("Status",               FieldType.MULT_CHOICE, "reg_status"));
+		s.add(new SearchParameter("Subject",              FieldType.MULT_CHOICE, "subject"));
+		s.add(new SearchParameter("Course Title Keyword", FieldType.TEXT_ENTRY,  "title"));
+		s.add(new SearchParameter("Instructor Last Name", FieldType.TEXT_ENTRY,  "instructor"));
+		s.add(new SearchParameter("General Education",    FieldType.MULT_CHOICE, "ge"));
+		s.add(new SearchParameter("Course Units",         FieldType.TEXT_ENTRY,  "crse_units_exact"));
+		s.add(new SearchParameter("Meeting Days",         FieldType.MULT_CHOICE, "Days"));
+		s.add(new SearchParameter("Meeting Times",        FieldType.MULT_CHOICE, "Times"));
 	}
 }
