2a5af8f7941bccbebc3d1edb327aa516b3aa671c
Maximillian Laumeister <max.laumeister@gmail.com>
2014-05-29 22:35:02 -0700

    Add new fields to SearchParameter; Add more comments

--- src/com/cmps121/ucsccoursebrowser/PisaHTMLModel.java
+++ src/com/cmps121/ucsccoursebrowser/PisaHTMLModel.java
@@ -2,14 +2,24 @@ package com.cmps121.ucsccoursebrowser;
 
 import java.util.ArrayList;
 
+// The PisaHTMLModel class acts as a global data structure, a kind of "database" for storing
+// each search parameter (which each include a label, info about how to display it to the user,
+// and the HTML ID that the HTMLGetter needs to look for when it parses the Pisa search page.
+
 import com.cmps121.ucsccoursebrowser.SearchParameter.FieldType;
 
 public class PisaHTMLModel {
 	
 	// We can iterate over SEARCH_PARAMETERS in MainActivity
-	// to populate the ListView items.
+	// to populate the ListView items with search parameters.
 	// This data structure will also be used by the search page parser
 	// to determine which ID's it needs to look for in the HTML.
+	
+	// This list of search parameters is hard-coded because they have been hand-selected
+	// as the most useful search parameters from the Pisa class search page.
+	// They are hard-coded because it would be more complicated to parse them from 
+	// the class search page, and such parsing would not be robust to future changes
+	// in the search page HTML.
 	
 	public static final ArrayList<SearchParameter> SEARCH_PARAMETERS = new ArrayList<SearchParameter>();
 	static {
--- src/com/cmps121/ucsccoursebrowser/SearchParameter.java
+++ src/com/cmps121/ucsccoursebrowser/SearchParameter.java
@@ -1,5 +1,12 @@
 package com.cmps121.ucsccoursebrowser;
 
+import java.util.ArrayList;
+
+// The SearchParameter class describes a single search parameter, for example "Term", "Subject", or "Instructor".
+// The complete, global list of these gets initialized by and stored in PisaHTMLModel before run time. When the app is run,
+// the HTMLGetter class immediately retrieves the Pisa search page and populates the 'value' and 'options' fields
+// of this global list.
+
 public class SearchParameter {
 	public enum FieldType {
 		MULT_CHOICE,
@@ -15,6 +22,16 @@ public class SearchParameter {
 	
 	// The id attribute of the HTML tag for this parameter in the class search page source.
 	public final String html_id;
+	
+	// The value attribute of the HTML option tag, e.g. for label "Computer Science",
+	// in the Pisa search page HTML source, the value would be "CMPS"
+	// This starts as null and gets populated when the HTMLGetter retrieves the search page
+	public String value;
+	
+	// The options for each search parameter - e.g. for "Term" the options would be
+	// ["2014 Fall Quarter", "2014 Summer Quarter", "2014 Spring Quarter", etc.]
+	// This starts as null and gets populated when the HTMLGetter retrieves the search page
+	public ArrayList<String> options;
 	
 	public SearchParameter(String lbl, FieldType t, String id) {
 		label = lbl;
