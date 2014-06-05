80d10e5bace923ddb1a46c4cda2763c79340f6b6
Maximillian Laumeister <max.laumeister@gmail.com>
2014-06-04 18:32:19 -0700

    Add pisa certificate as trusted

--- src/com/cmps121/ucsccoursebrowser/HTMLGetter.java
+++ src/com/cmps121/ucsccoursebrowser/HTMLGetter.java
@@ -1,15 +1,38 @@
 package com.cmps121.ucsccoursebrowser;
 
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 
 import android.content.Context;
 import android.os.AsyncTask;
@@ -19,12 +42,13 @@ public abstract class HTMLGetter extends AsyncTask<HttpUriRequest, Void, String>
 	
 	private static final String LOG_TAG = MainActivity.LOG_TAG;
 	
-	private static DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+	private DefaultHttpClient httpclient;
 	
 	protected Context ctx;
 	
 	public HTMLGetter(Context context) {
 		ctx = context;
+		httpclient = getNewClient();
 	}
 	
 	// doInBackground(HttpPost request):
@@ -45,7 +69,7 @@ public abstract class HTMLGetter extends AsyncTask<HttpUriRequest, Void, String>
 		    if (httppost instanceof HttpPost) {
 		    	response = httpclient.execute(httppost);
 		    } else {
-		    	response = (new DefaultHttpClient(new BasicHttpParams())).execute(httppost);
+		    	response = getNewClient().execute(httppost);
 		    }
 		    HttpEntity entity = response.getEntity();
 
@@ -69,4 +93,131 @@ public abstract class HTMLGetter extends AsyncTask<HttpUriRequest, Void, String>
 		}
 		return "";
 	}
+	
+	private DefaultHttpClient getNewClient() {
+		final SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		schemeRegistry.register(new Scheme("https", createAdditionalCertsSSLSocketFactory(), 443));
+		final HttpParams params = new BasicHttpParams();
+		final ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(params,schemeRegistry);
+		return new DefaultHttpClient(cm, new BasicHttpParams());
+	}
+	
+	protected org.apache.http.conn.ssl.SSLSocketFactory createAdditionalCertsSSLSocketFactory() {
+	    try {
+	        final KeyStore ks = KeyStore.getInstance("BKS");
+
+	        // the bks file we generated above
+	        final InputStream in = ctx.getResources().openRawResource( R.raw.pisa_cert);  
+	        try {
+	            ks.load(in, ctx.getString( R.string.incommon_store_password ).toCharArray());
+	        } finally {
+	            in.close();
+	        }
+
+	        return new AdditionalKeyStoresSSLSocketFactory(ks);
+
+	    } catch( Exception e ) {
+	        throw new RuntimeException(e);
+	    }
+	}
+	
+	/**
+	 * Allows you to trust certificates from additional KeyStores in addition to
+	 * the default KeyStore
+	 */
+	public class AdditionalKeyStoresSSLSocketFactory extends SSLSocketFactory {
+	    protected SSLContext sslContext = SSLContext.getInstance("TLS");
+
+	    public AdditionalKeyStoresSSLSocketFactory(KeyStore keyStore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+	        super(null, null, null, null, null, null);
+	        sslContext.init(null, new TrustManager[]{new AdditionalKeyStoresTrustManager(keyStore)}, null);
+	    }
+
+	    @Override
+	    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException {
+	        return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+	    }
+
+	    @Override
+	    public Socket createSocket() throws IOException {
+	        return sslContext.getSocketFactory().createSocket();
+	    }
+
+	}
+	
+	/**
+     * Based on http://download.oracle.com/javase/1.5.0/docs/guide/security/jsse/JSSERefGuide.html#X509TrustManager
+     */
+    public static class AdditionalKeyStoresTrustManager implements X509TrustManager {
+
+        protected ArrayList<X509TrustManager> x509TrustManagers = new ArrayList<X509TrustManager>();
+
+
+        protected AdditionalKeyStoresTrustManager(KeyStore... additionalkeyStores) {
+            final ArrayList<TrustManagerFactory> factories = new ArrayList<TrustManagerFactory>();
+
+            try {
+                // The default Trustmanager with default keystore
+                final TrustManagerFactory original = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                original.init((KeyStore) null);
+                factories.add(original);
+
+                for( KeyStore keyStore : additionalkeyStores ) {
+                    final TrustManagerFactory additionalCerts = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                    additionalCerts.init(keyStore);
+                    factories.add(additionalCerts);
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+            /*
+             * Iterate over the returned trustmanagers, and hold on
+             * to any that are X509TrustManagers
+             */
+            for (TrustManagerFactory tmf : factories)
+                for( TrustManager tm : tmf.getTrustManagers() )
+                    if (tm instanceof X509TrustManager)
+                        x509TrustManagers.add( (X509TrustManager)tm );
+
+
+            if( x509TrustManagers.size()==0 )
+                throw new RuntimeException("Couldn't find any X509TrustManagers");
+
+        }
+
+        /*
+         * Delegate to the default trust manager.
+         */
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            final X509TrustManager defaultX509TrustManager = x509TrustManagers.get(0);
+            defaultX509TrustManager.checkClientTrusted(chain, authType);
+        }
+
+        /*
+         * Loop over the trustmanagers until we find one that accepts our server
+         */
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            for( X509TrustManager tm : x509TrustManagers ) {
+                try {
+                    tm.checkServerTrusted(chain,authType);
+                    return;
+                } catch( CertificateException e ) {
+                    // ignore
+                }
+            }
+            throw new CertificateException();
+        }
+
+        public X509Certificate[] getAcceptedIssuers() {
+            final ArrayList<X509Certificate> list = new ArrayList<X509Certificate>();
+            for( X509TrustManager tm : x509TrustManagers )
+                list.addAll(Arrays.asList(tm.getAcceptedIssuers()));
+            return list.toArray(new X509Certificate[list.size()]);
+        }
+    }
 }
